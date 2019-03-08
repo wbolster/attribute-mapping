6663b1e750654f9fa8530e2bbadbc0f326af9ac0
wouter bolsterlee <wouter@bolsterl.ee>
2019-03-08 22:52:29 +0100

    split tests

--- test_attribute_mapping.py
+++ test_attribute_mapping.py
@@ -1,47 +1,80 @@
 import pytest
+
 from attribute_mapping import AttributeMapping
 
 
-def test_attribute_mapping():
+def test_get_original():
+    d = {}
+    x = AttributeMapping(d)
 
+    assert vars(x) == d
+    assert vars(x) is d
+
+
+def test_attributes():
     d = {"a": 1, "b": {"c": 2, "d": 3}}
-    ad = AttributeMapping(d)
+    x = AttributeMapping(d)
 
-    assert ad.a == 1
-    assert ad.b
-    assert ad.b.c == 2
-
-    assert ad["a"] == 1
-    assert ad.b["d"] == 3
+    assert x.a == 1
+    assert x.b
+    assert x.b.c == 2
 
     with pytest.raises(AttributeError):
-        ad.nonexistent
+        x.nonexistent
+
+    x.foo = 123
+    assert x.foo == 123
+    assert hasattr(x, "foo")
+
+    del x.foo
+    assert not hasattr(x, "foo")
+
+
+def test_items():
+    d = {"a": 1, "b": {"c": 2, "d": 3}}
+    x = AttributeMapping(d)
+
+    assert x["a"] == 1
+    assert x.b["d"] == 3
 
     with pytest.raises(KeyError):
-        ad["nonexistent"]
+        x["nonexistent"]
 
-    assert vars(ad) == d
-    assert vars(ad) is d
+    x["foo"] = 456
+    assert x["foo"] == 456
+    assert "foo" in x
 
-    ad.foo = 123
-    assert ad.foo == 123
-    ad["foo"] = 456
-    assert ad.foo == 456
-
-    assert hasattr(ad, "foo")
-    assert "foo" in ad
-    del ad.foo
-    assert not hasattr(ad, "foo")
-    assert "foo" not in ad
-    ad.foo = 123
-    del ad["foo"]
-    assert not hasattr(ad, "foo")
-    assert "foo" not in ad
+    del x["foo"]
+    assert "foo" not in x
 
 
 def test_nested_instance():
-    a = AttributeMapping({})
-    a.foo = AttributeMapping({})
-    a.foo.bar = 1
+    x = AttributeMapping({})
+    x.foo = AttributeMapping({})
+    x.foo.bar = 1
+    assert vars(x) == {"foo": {"bar": 1}}
+
+
+def test_repr():
+    x = AttributeMapping({"foo": {"bar": 1}})
     expected = "AttributeMapping({'foo': {'bar': 1}})"
-    assert repr(a) == expected
+    assert repr(x) == str(x) == expected
+
+
+def test_iteration():
+    d = {"foo": "bar"}
+    x = AttributeMapping(d)
+    for key, value in x:
+        assert key == "foo"
+        assert value == "bar"
+
+    d = {"a": 1, "b": 2}
+    x = AttributeMapping(d)
+    assert sorted(x) == [("a", 1), ("b", 2)]
+
+
+def test_special_attributes():
+    d = {"foo": "bar"}
+    x = AttributeMapping(d)
+    assert x.__class__ is AttributeMapping
+    assert x.__dict__ is d
